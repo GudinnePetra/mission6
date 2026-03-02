d4ffb53ea2e361a0728376fbc4304b1f047e9541
Esma Aleyna Üstündağ <esmaaleyna4680@gmail.com>
2026-03-02 16:21:31 +0300

    Initial commit

---
+++ Assets/AAAA/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] objectsPrefab;
+    public Vector3 spawnPos = new Vector3(-0.200000003f, 3.00999999f, -0.75999999f);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
